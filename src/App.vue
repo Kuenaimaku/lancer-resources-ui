9910ec4cbb3d88232ce8dcf9643dcced4ce941a6
kuenaimaku <kuenaimaku@gmail.com>
2021-10-29 15:35:54 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <p class="title">
+        Lancer Resources
+      </p>
+      <p class="subtitle">
+        Primary subtitle
+      </p>
+    </div>
+  </section>
+  <section class="section">
+    <div class="container">
+      
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  setup() {
+    const getResources = useGetResults()
+    const productSearch = useSearch()
+    const resultSorting = useSorting({})
+    return { getResources, productSearch, resultSorting }
+  }
+}
+function getResults(){
+  fetch()
+}
+function useSearch(getResults) { 
+}
+function useSorting({ input, options }) { 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.container{
+  display:flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  column-gap: 1em;
+  align-items: flex-start;
+}
+
+
+</style>
---
+++ src/components/Resource.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="box">
+		<div class="card">
+			<div class="card-content">
+				<div class="media">
+					<div class="media-content">
+						<p class="title is-4">Discord Username</p>
+					</div>
+				</div>
+
+				<div class="content">
+					Text
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'Resource',
+  components: { },
+	props:{
+		resource: Object,
+	},
+  setup(props) {
+
+  }
+});
+</script>
+
+
+<style scoped>
+
+.box {
+	max-width: 20vw;
+}
+
+</style>
